1719ea4d7f9d7882d4ca071095c96bc74c992052
Karol <dabrowski.karad@gmail.com>
2019-05-09 17:30:04 +0200

    Add home page component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="page-container">
+            Temporary home page content
+        </div>
+    );
+};
+
+export default HomePage;
