26cb4425fecdbdc2bc9b1fd58b89964e9f323782
Karol <dabrowski.karad@gmail.com>
2019-05-13 20:18:38 +0200

    Add thunk middleware to the store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
 import App from './components/App';
-import rootReducer from './reducers'
+import rootReducer from './reducers';
 
 const store = createStore(
     rootReducer,
     {},
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+        applyMiddleware(thunkMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 ReactDOM.render(
