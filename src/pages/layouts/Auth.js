3e5ac999dcfe3c162c275f9379f080dc2f8f29b0
Karol <dabrowski.karad@gmail.com>
2019-05-22 15:19:07 +0200

    Add redirection from Auth component when user is logged in

--- src/pages/layouts/Auth.js
+++ src/pages/layouts/Auth.js
@@ -1,9 +1,30 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
+import {connect} from "react-redux";
 
 class Auth extends Component {
+    componentDidMount() {
+        const {history, authorized} = this.props;
+        if(authorized) {
+            history.push('/dashboard');
+        }
+    }
+
+    componentDidUpdate() {
+        const {history, authorized} = this.props;
+        if(authorized) {
+            history.push('/dashboard');
+        }
+    }
+
     render() {
         return 'Auth page layout'
     }
 }
 
-export default Auth;
+const mapStateToProps = (state) => {
+    return {
+        authorized: state.auth.authorized
+    };
+};
+
+export default connect(mapStateToProps)(Auth);
