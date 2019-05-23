9d4ca4027b4449a974bea53f8d5fba1d00c5d23c
Karol <dabrowski.karad@gmail.com>
2019-05-23 12:19:18 +0200

    Implement Static layout

--- src/pages/layouts/Static.js
+++ src/pages/layouts/Static.js
@@ -2,17 +2,29 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import HomePage from "../HomePage";
+import {withStyles} from "@material-ui/core";
 
 class Static extends Component {
     render() {
+        const {classes} = this.props;
+
         return (
-            <Router>
-                <Switch>
-                    <Route path="/" component={HomePage} exact />
-                </Switch>
-            </Router>
+            <div className={classes.staticContainer}>
+                <Router>
+                    <Switch>
+                        <Route path="/" component={HomePage} exact />
+                    </Switch>
+                </Router>
+            </div>
         );
     }
 }
 
-export default Static;
+const styles = theme => ({
+    staticContainer: {
+        backgroundColor: theme.palette.background.default,
+        width: '100%'
+    },
+});
+
+export default withStyles(styles)(Static);
