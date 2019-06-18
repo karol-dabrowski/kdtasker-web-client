a3966c1bad9532be58f1a6a949d504ed8420a652
Karol <dabrowski.karad@gmail.com>
2019-06-18 20:05:45 +0200

    Implement reducer for today's tasks

--- src/reducers/todaysTasks.js
+++ src/reducers/todaysTasks.js
@@ -1,10 +1,27 @@
+import {GET_TASKS_FOR_TODAY_REQUEST, GET_TASKS_FOR_TODAY_SUCCESS, GET_TASKS_FOR_TODAY_FAILURE} from '../actions/taskActions';
+
 const defaultState = {
     loading: false,
+    date: null,
     list: []
 };
 
 const todaysTasks = (state = defaultState, action) => {
     switch (action.type) {
+        case GET_TASKS_FOR_TODAY_REQUEST:
+            return Object.assign({}, state, {
+                loading: true
+            });
+        case GET_TASKS_FOR_TODAY_SUCCESS:
+            return Object.assign({}, state, {
+                loading: false,
+                date: action.data.date,
+                list: action.data.tasks_list
+            });
+        case GET_TASKS_FOR_TODAY_FAILURE:
+            return Object.assign({}, state, {
+                loading: false
+            });
         default:
             return state;
     }
