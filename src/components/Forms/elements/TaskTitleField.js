d54b4fe165b361d6b30615a2eb9471b1d3bfa14f
Karol <dabrowski.karad@gmail.com>
2019-06-20 20:26:15 +0200

    Create Title field for task forms

---
+++ src/components/Forms/elements/TaskTitleField.js
@@ -0,0 +1,40 @@
+import React, {Fragment} from "react";
+import {TextField, withStyles} from "@material-ui/core";
+import {ErrorMessage} from "formik";
+
+const TitleField = (props) => {
+    const {classes, handleChange, handleBlur, value, touched, error} = props;
+    return (
+        <Fragment>
+            <TextField
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={value}
+                variant="outlined"
+                id="title"
+                margin="normal"
+                label="Title"
+                name="title"
+                fullWidth
+                autoFocus
+                className={touched && error ? classes.errorField : ''}
+            />
+            <ErrorMessage className={classes.errorMessage} component="div" name="title" />
+        </Fragment>
+    );
+};
+
+const style = theme => ({
+    errorMessage: {
+        color: theme.palette.error.main
+    },
+    errorField: {
+        '& .MuiOutlinedInput-root': {
+            '& fieldset, &:hover fieldset, &.Mui-focused fieldset': {
+                borderColor: theme.palette.error.main
+            }
+        }
+    }
+});
+
+export default withStyles(style)(TitleField);
