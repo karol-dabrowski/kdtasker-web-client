dd8d585aaa88c457ea2bfd8ea21b0465c2ad3bcb
Karol <dabrowski.karad@gmail.com>
2019-06-21 18:11:30 +0200

    Implement dateTime provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
 import App from './components/App';
 import rootReducer from './reducers';
 import theme from './theme';
@@ -19,9 +22,11 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider theme={theme}>
-            <App />
-        </MuiThemeProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
+        </MuiPickersUtilsProvider>
     </Provider>,
     document.getElementById('root')
 );
