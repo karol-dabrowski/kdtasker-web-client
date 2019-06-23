73c3cbb18f7c2271a6e2c279be99be3b02154933
Karol <dabrowski.karad@gmail.com>
2019-06-23 23:10:50 +0200

    Create reducer for new task action

---
+++ src/reducers/createTask.js
@@ -0,0 +1,26 @@
+import {CREATE_TASK_REQUEST, CREATE_TASK_SUCCESS, CREATE_TASK_FAILURE} from '../actions/taskActions';
+
+const defaultState = {
+    loading: false
+};
+
+const createTask = (state = defaultState, action) => {
+    switch (action.type) {
+        case CREATE_TASK_REQUEST:
+            return Object.assign({}, state, {
+                loading: true
+            });
+        case CREATE_TASK_SUCCESS:
+            return Object.assign({}, state, {
+                loading: false
+            });
+        case CREATE_TASK_FAILURE:
+            return Object.assign({}, state, {
+                loading: false
+            });
+        default:
+            return state;
+    }
+};
+
+export default createTask;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import todaysTasks from './todaysTasks'
 import auth from './auth'
+import createTask from './createTask'
 
 export default combineReducers({
     auth,
-    todaysTasks
+    todaysTasks,
+    createTask
 });
