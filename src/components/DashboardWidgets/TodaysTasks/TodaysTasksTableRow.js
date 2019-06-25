987a30f6c435477870df4124849476cd2c231072
Karol <dabrowski.karad@gmail.com>
2019-06-25 12:59:15 +0200

    Add action icons to tasks table

--- src/components/DashboardWidgets/TodaysTasks/TodaysTasksTableRow.js
+++ src/components/DashboardWidgets/TodaysTasks/TodaysTasksTableRow.js
@@ -1,6 +1,10 @@
 import React from "react";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import IconButton from '@material-ui/core/IconButton';
+import Done from '@material-ui/icons/Done';
+import Edit from '@material-ui/icons/Edit';
+import Delete from '@material-ui/icons/Delete';
 
 const TodaysTasksTableRow = props => {
     const {task} = props;
@@ -9,7 +13,17 @@ const TodaysTasksTableRow = props => {
         <TableRow>
             <TableCell align="left">{task.time}</TableCell>
             <TableCell align="left">{task.title}</TableCell>
-            <TableCell align="left">@</TableCell>
+            <TableCell align="left">
+                <IconButton aria-label="Done">
+                    <Done />
+                </IconButton>
+                <IconButton aria-label="Edit">
+                    <Edit />
+                </IconButton>
+                <IconButton aria-label="Delete">
+                    <Delete />
+                </IconButton>
+            </TableCell>
         </TableRow>
     );
 };
