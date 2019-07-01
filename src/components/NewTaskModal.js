85d26b08017fa691183ac4ff08f15ed2575ddaf5
Karol <dabrowski.karad@gmail.com>
2019-07-01 14:24:30 +0200

    Remove redundant statements from NewTaskModal

--- src/components/NewTaskModal.js
+++ src/components/NewTaskModal.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {withStyles} from "@material-ui/core";
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -29,14 +28,10 @@ class NewTaskModal extends Component {
     }
 }
 
-const styles = theme => ({
-
-});
-
 const mapStateToProps = (state) => {
     return {
         loading: state.createTask.loading
     };
 };
 
-export default connect(mapStateToProps)(withStyles(styles)(NewTaskModal));
+export default connect(mapStateToProps)(NewTaskModal);
