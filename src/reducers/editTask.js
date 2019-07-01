100059532714b8be4327dcd028a666797e337d18
Karol <dabrowski.karad@gmail.com>
2019-07-01 14:57:39 +0200

    Add editTask reducer

---
+++ src/reducers/editTask.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    loading: false
+};
+
+const editTask = (state = defaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default editTask;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import todaysTasks from './todaysTasks'
 import auth from './auth'
 import createTask from './createTask'
+import editTask from './editTask'
 
 export default combineReducers({
     auth,
     todaysTasks,
-    createTask
+    createTask,
+    editTask
 });
