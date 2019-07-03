89f7c1bef7791feee93ca2c955b426213bee5308
Karol <dabrowski.karad@gmail.com>
2019-07-03 21:53:37 +0200

    Implement task edition in reducer

--- src/reducers/todaysTasks.js
+++ src/reducers/todaysTasks.js
@@ -5,6 +5,9 @@ import {
     CREATE_TASK_REQUEST,
     CREATE_TASK_SUCCESS,
     CREATE_TASK_FAILURE,
+    EDIT_TASK_REQUEST,
+    EDIT_TASK_SUCCESS,
+    EDIT_TASK_FAILURE,
     COMPLETE_TASK_REQUEST,
     COMPLETE_TASK_SUCCESS,
     COMPLETE_TASK_FAILURE,
@@ -53,6 +56,40 @@ const todaysTasks = (state = defaultState, action) => {
             }
             return newStateForCreateTaskSuccess;
         case CREATE_TASK_FAILURE:
+            return Object.assign({}, state, {
+                loading: false
+            });
+        case EDIT_TASK_REQUEST:
+            return Object.assign({}, state, {
+                loading: true
+            });
+        case EDIT_TASK_SUCCESS:
+            const editedTaskDate = new Date(action.taskData.deadline_date);
+            let editTasksList;
+            if(editedTaskDate.toDateString() === (new Date()).toDateString()) {
+                const editedTaskIndex = state.list.findIndex(task => {
+                    return task.task_id === action.taskId
+                });
+                editTasksList = state.list;
+                editTasksList[editedTaskIndex].title = action.taskData.title;
+                editTasksList[editedTaskIndex].deadline_date = action.taskData.deadline_date;
+                editTasksList[editedTaskIndex].deadline_time = action.taskData.deadline_time;
+                editTasksList.sort((a, b) => {
+                    const result = a.deadline_time === null && b.deadline_time !== null
+                        ? true
+                        : a.deadline_time > b.deadline_time;
+                    return +result;
+                });
+            } else {
+                editTasksList = state.list.filter(task => {
+                    return task.task_id !== action.taskId;
+                })
+            }
+            return  Object.assign({}, state, {
+                loading: false,
+                list: editTasksList
+            });
+        case EDIT_TASK_FAILURE:
             return Object.assign({}, state, {
                 loading: false
             });
