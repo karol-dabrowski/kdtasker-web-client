fcde17fc984b71de7c2557f62d9199e1575913b7
Karol <dabrowski.karad@gmail.com>
2019-07-04 10:04:57 +0200

    Remove redundant Login component and move it's features to LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import Login from "../components/containers/Login";
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 
-const LoginPage = () => {
-    return (
-        <Login />
-    );
+import LoginForm from "../components/Forms/LoginForm";
+import {login} from "../actions/authActions";
+import AuthWrapper from "../components/AuthWrapper";
+import Preloader from "../components/Preloader";
+
+class LoginPage extends Component {
+    handleLoginFormSubmit = (email, password) => {
+        this.props.dispatch(login(email, password));
+    };
+
+    render() {
+        const {loading} = this.props;
+
+        if(loading) {
+            return <Preloader />;
+        } else {
+            const formTitle = 'Sign in';
+            return (
+                <AuthWrapper title={formTitle}>
+                    <LoginForm handleSubmit={this.handleLoginFormSubmit} />
+                </AuthWrapper>
+            );
+        }
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading
+    };
 };
 
-export default LoginPage;
+export default connect(mapStateToProps)(LoginPage);
