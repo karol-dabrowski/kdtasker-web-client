dcda1b107793c5b7d3fc2b50d9a816f4e1c6eecf
Karol <dabrowski.karad@gmail.com>
2019-07-05 16:50:59 +0200

    Implement simple cases to nextDaysTasks reducer

--- src/reducers/nextDaysTasks.js
+++ src/reducers/nextDaysTasks.js
@@ -1,9 +1,29 @@
+import {
+    GET_TASKS_FOR_NEXT_DAYS_REQUEST,
+    GET_TASKS_FOR_NEXT_DAYS_SUCCESS,
+    GET_TASKS_FOR_NEXT_DAYS_FAILURE
+} from '../actions/taskActions';
+
 const defaultState = {
-    loading: false
+    loading: false,
+    list: []
 };
 
 const nextDaysTasks = (state = defaultState, action) => {
     switch (action.type) {
+        case GET_TASKS_FOR_NEXT_DAYS_REQUEST:
+            return Object.assign({}, state, {
+                loading: true
+            });
+        case GET_TASKS_FOR_NEXT_DAYS_SUCCESS:
+            return Object.assign({}, state, {
+                loading: false,
+                list: action.data ? action.data : []
+            });
+        case GET_TASKS_FOR_NEXT_DAYS_FAILURE:
+            return Object.assign({}, state, {
+                loading: false
+            });
         default:
             return state;
     }
