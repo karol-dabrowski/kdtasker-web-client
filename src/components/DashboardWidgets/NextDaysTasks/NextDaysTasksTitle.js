285809f0fd3140333d263c4c4981159a7e790963
Karol <dabrowski.karad@gmail.com>
2019-07-07 16:37:13 +0200

    Create title component for day's list

---
+++ src/components/DashboardWidgets/NextDaysTasks/NextDaysTasksTitle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {withStyles} from "@material-ui/core";
+
+const NextDaysTasksTitle = props => {
+    const {classes, date, dayOfTheWeek} = props;
+
+    return <h4 className={classes.dayTableHeader}>{dayOfTheWeek} - {date}</h4>;
+};
+
+const styles = theme => ({
+    dayTableHeader: {
+        marginTop: theme.spacing(0)
+    }
+});
+
+export default withStyles(styles)(NextDaysTasksTitle);
