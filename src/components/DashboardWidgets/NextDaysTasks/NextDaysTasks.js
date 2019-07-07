4c78f9c22840c9e98dfd030bf8ff0f79c8ab95bd
Karol <dabrowski.karad@gmail.com>
2019-07-07 16:54:56 +0200

    Implement tasks list in NextDaysTasks dashboard widget

--- src/components/DashboardWidgets/NextDaysTasks/NextDaysTasks.js
+++ src/components/DashboardWidgets/NextDaysTasks/NextDaysTasks.js
@@ -1,18 +1,40 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+
 import WidgetWrapper from "../WidgetWrapper";
+import NextDaysTasksTable from "./NextDaysTasksTable";
+import {getTasksForNextDays} from "../../../actions/taskActions";
 import Preloader from "../../Preloader";
 
 class NextDaysTasks extends Component {
+    componentDidMount() {
+        this.props.dispatch(getTasksForNextDays(this.props.jwtToken, 7));
+    };
+
     render() {
-        const {loading} = this.props;
+        const {loading, list} = this.props;
+
+        const sortedTaskList = list.reduce((accumulator, task) => {
+            if(!accumulator.hasOwnProperty(task.deadline_date)) {
+                accumulator[task.deadline_date] = [];
+            }
+            accumulator[task.deadline_date].push(task);
+            return accumulator;
+        }, {});
+
+        const dailyLists = Object.keys(sortedTaskList).map(day =>
+            <NextDaysTasksTable key={day} day={day} dayTasks={sortedTaskList[day]} />
+        );
 
         return (
-            <WidgetWrapper title="Next days" loading={loading} refreshButton={true}>
+            <WidgetWrapper title="Next 7 days" loading={loading} refreshButton={true}>
                 { loading ? (
                         <Preloader />
-                    ) :
-                    ' '
+                    ) : (
+                        list.length > 0 ? (
+                            dailyLists
+                        ) : 'You don\'t have any tasks for next 7 days'
+                    )
                 }
             </WidgetWrapper>
         );
@@ -22,6 +44,7 @@ class NextDaysTasks extends Component {
 const mapStateToProps = (state) => {
     return {
         loading: state.nextDaysTasks.loading,
+        list: state.nextDaysTasks.list,
         jwtToken: state.auth.jwt.token
     };
 };
