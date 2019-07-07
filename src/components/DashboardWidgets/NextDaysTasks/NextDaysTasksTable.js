f569cd87e2dc26ec5962062f7e3f0a0de6563292
Karol <dabrowski.karad@gmail.com>
2019-07-07 16:54:17 +0200

    Create table component for next days tasks widget

---
+++ src/components/DashboardWidgets/NextDaysTasks/NextDaysTasksTable.js
@@ -0,0 +1,29 @@
+import React, {Fragment} from 'react';
+import TableBody from "@material-ui/core/TableBody";
+import Table from "@material-ui/core/Table";
+
+import NextDaysTasksTitle from "./NextDaysTasksTitle";
+import NextDaysTasksTableRow from "./NextDaysTasksTableRow";
+
+const NextDaysTasksTable = props => {
+    const {day, dayTasks} = props;
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayOfTheWeek = (new Date(day)).getDay();
+
+    const rows = dayTasks.map(task =>
+        <NextDaysTasksTableRow task={task} />
+    );
+
+    return (
+        <Fragment>
+            <NextDaysTasksTitle date={day} dayOfTheWeek={weekdays[currentDayOfTheWeek]} />
+            <Table>
+                <TableBody>
+                    {rows}
+                </TableBody>
+            </Table>
+        </Fragment>
+    );
+};
+
+export default NextDaysTasksTable;
