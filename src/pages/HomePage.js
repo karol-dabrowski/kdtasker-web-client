51e498ce61cc3f825d939991ddc86847a2ceddb8
Karol <dabrowski.karad@gmail.com>
2019-07-08 11:21:50 +0200

    Create temporary home page content

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,36 @@
-const HomePage = () => {
+import React from "react";
+import {withStyles} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {Link} from "react-router-dom";
+
+const HomePage = props => {
+    const {classes} = props;
+
     return (
-        'Temporary home page content'
+        <div className={classes.temporaryContentWrapper}>
+            <h3>Home page under construction.</h3>
+            <p>You can register or login to the alpha version of the dashboard</p>
+            <Button className={classes.temporaryContentLoginButton} color="primary" variant="outlined" component={Link} to="/login">
+                Sign in
+            </Button>
+            <Button className={classes.temporaryContentRegisterButton} color="primary" variant="outlined" component={Link} to="/register">
+                Sign up
+            </Button>
+        </div>
     );
 };
 
-export default HomePage;
+const styles = theme => ({
+    temporaryContentWrapper: {
+        textAlign: 'center',
+        marginTop: theme.spacing(10)
+    },
+    temporaryContentLoginButton: {
+        marginRight: theme.spacing(1)
+    },
+    temporaryContentRegisterButton: {
+        marginLeft: theme.spacing(1)
+    }
+});
+
+export default withStyles(styles)(HomePage);
