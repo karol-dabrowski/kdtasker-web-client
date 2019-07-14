b1c2239ea0144acd5269b59711803a29fd97fcf5
Karol <dabrowski.karad@gmail.com>
2019-07-14 16:37:36 +0200

    Finish SidebarTitle component

--- src/components/Sidebar/SidebarTitle.js
+++ src/components/Sidebar/SidebarTitle.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { withStyles } from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
 
 const SidebarTitle = props => {
-    const { t } = props;
+    const { classes, t } = props;
 
     return (
-        <div>{t("header.brand")}</div>
+        <Fragment>
+            <div className={classes.sidebarTitle}>
+                {t("header.brand")}
+                <Divider className={classes.sidebarTitleDivider} component="hr" />
+            </div>
+        </Fragment>
     );
 };
 
-export default SidebarTitle;
+const styles = theme => ({
+    sidebarTitle: {
+        width: "90%",
+        textAlign: "center",
+        margin: "auto",
+        marginTop: theme.spacing(2),
+        color: theme.palette.common.white
+    },
+    sidebarTitleDivider: {
+        marginTop: theme.spacing(1),
+        backgroundColor: "white"
+    }
+});
+
+export default withStyles(styles)(SidebarTitle);
