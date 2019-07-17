f928112ba7de02fabbd9a8c836acdf3287e4e59d
Karol <dabrowski.karad@gmail.com>
2019-07-17 14:44:08 +0200

    Refactor EDIT_TASK_SUCCESS case in todaysTasks reducer

--- src/reducers/todaysTasks.js
+++ src/reducers/todaysTasks.js
@@ -63,29 +63,25 @@ const todaysTasks = (state = defaultState, action) => {
                 loading: true
             });
         case EDIT_TASK_SUCCESS:
-            const editedTaskDate = new Date(action.taskData.deadline_date);
-            let editTasksList;
-            if (editedTaskDate.toDateString() === new Date().toDateString()) {
-                const editedTaskIndex = state.list.findIndex(task => {
-                    return task.task_id === action.taskId;
-                });
-                editTasksList = state.list;
-                editTasksList[editedTaskIndex].title = action.taskData.title;
-                editTasksList[editedTaskIndex].deadline_date = action.taskData.deadline_date;
-                editTasksList[editedTaskIndex].deadline_time = action.taskData.deadline_time;
-                editTasksList.sort((a, b) => {
-                    const result =
-                        a.deadline_time === null && b.deadline_time !== null ? true : a.deadline_time > b.deadline_time;
-                    return +result;
-                });
-            } else {
-                editTasksList = state.list.filter(task => {
-                    return task.task_id !== action.taskId;
-                });
-            }
             return Object.assign({}, state, {
                 loading: false,
-                list: editTasksList
+                list:
+                    new Date(action.taskData.deadline_date).toDateString() === new Date().toDateString()
+                        ? state.list
+                              .filter(task => {
+                                  return task.task_id !== action.taskId;
+                              })
+                              .concat(Object.assign({ task_id: action.taskId }, action.taskData))
+                              .sort((a, b) => {
+                                  let result =
+                                      a.deadline_time === null && b.deadline_time !== null
+                                          ? true
+                                          : a.deadline_time > b.deadline_time;
+                                  return +result;
+                              })
+                        : state.list.filter(task => {
+                              return task.task_id !== action.taskId;
+                          })
             });
         case EDIT_TASK_FAILURE:
             return Object.assign({}, state, {
